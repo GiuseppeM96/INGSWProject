751e2ef9c22d11522a7253892fb33b741d8c8398
mattzav <matteozav@hotmail.com>
2017-11-18 11:38:02 +0100

    Some methods added

--- src/TestingClass.java
+++ src/TestingClass.java
@@ -1,38 +1,54 @@
 
 public class TestingClass {
 	public int[] array;
-	
+
 	public TestingClass(int size) {
-		array=new int[size];
+		array = new int[size];
 	}
-	
+
 	public int min() {
-		int min=array[0];
-		int sumOcc=0;
-		
-		for(int i=0;i<array.length;i++) {
-			if(array[i]<min) {
-				min=array[i];
-				sumOcc=min;
-			}else if(array[i]==min) {
-				sumOcc+=min;
+		int min = array[0];
+		int sumOcc = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] < min) {
+				min = array[i];
+				sumOcc = min;
+			} else if (array[i] == min) {
+				sumOcc += min;
 			}
 		}
-		
-		if(sumOcc!=min) {
+
+		if (sumOcc != min) {
 			return sumOcc;
 		}
 		return min;
 	}
-	
+
 	public int max() {
-		
-		int max=0;
+
+		int max = 0;
 		for (int i = 0; i < array.length; i++) {
-			if(array[i]>max) {
-				max=array[i];
+			if (array[i] > max) {
+				max = array[i];
 			}
 		}
+		return max;
+	}
+
+	public int min_zav() {
+		int min = array[0];
+		for (int i = 1; i < array.length; i++)
+			if (array[i] < min)
+				min = array[i];
+		return min;
+	}
+
+	public int max_zav() {
+		int max = array[0];
+		for (int i = 1; i < array.length; i++)
+			if (array[i] > max)
+				max = array[i];
 		return max;
 	}
 }
